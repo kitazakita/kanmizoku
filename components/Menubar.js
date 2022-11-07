60d77e4c07f5d40cc39cf7e4b701225c6e59c364
“kitazakita” <kitazaki.webdesign@gmail.com>
2022-11-07 12:03:33 +0900

    メニューバー完成

--- components/Menubar.js
+++ components/Menubar.js
@@ -1,12 +1,22 @@
 import Link from "next/link";
 import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import { useState } from "react";
 
 const Menubar = () => {
+  const [mainMenuToggle, setMainMenuToggle] = useState(false);
+
+  const clickMenuToggle = () => {
+    setMainMenuToggle((prev) => {
+      return !prev;
+    });
+  };
+
   return (
     <>
+      <MainMenu display={mainMenuToggle ? true : false} />
       <div className="menu-bar">
-        <div className="menu-button">
-          <div className="menu-box">
+        <div className="menu-button" onClick={clickMenuToggle}>
+          <div className={mainMenuToggle ? "menu-box active" : "menu-box"}>
             <span></span>
             <span></span>
             <span></span>
@@ -22,6 +32,28 @@ const Menubar = () => {
             <p>TOP</p>
           </div>
         </div>
+      </div>
+    </>
+  );
+};
+
+const MainMenu = (props) => {
+  return (
+    <>
+      <div className={props.display ? "main-menu active" : "main-menu"}>
+        <nav className="g-nav">
+          <ul>
+            <li>
+              <Link href="#">商品一覧</Link>
+            </li>
+            <li>
+              <Link href="#">アクセス</Link>
+            </li>
+            <li>
+              <Link href="#">ギャラリー</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
