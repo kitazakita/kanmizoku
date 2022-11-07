24acc2488f87395d9f4bf1a8b9de2267fe002142
“kitazakita” <kitazaki.webdesign@gmail.com>
2022-11-07 12:36:46 +0900

    メニュー展開時のpointerEvents修正

--- components/Menubar.js
+++ components/Menubar.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
-import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
 import { useState } from "react";
+import { Link as Scroll } from "react-scroll";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
 
 const Menubar = () => {
   const [mainMenuToggle, setMainMenuToggle] = useState(false);
@@ -25,12 +26,14 @@ const Menubar = () => {
         </div>
 
         <div className="page-top-button">
-          <div className="top-button">
-            <div className="top-box">
-              <KeyboardDoubleArrowUpIcon />
+          <Scroll to="scroll-top" smooth={true}>
+            <div className="top-button">
+              <div className="top-box">
+                <KeyboardDoubleArrowUpIcon />
+              </div>
+              <p>TOP</p>
             </div>
-            <p>TOP</p>
-          </div>
+          </Scroll>
         </div>
       </div>
     </>
@@ -40,17 +43,23 @@ const Menubar = () => {
 const MainMenu = (props) => {
   return (
     <>
-      <div className={props.display ? "main-menu active" : "main-menu"}>
+      <div
+        id="scroll-top"
+        className={props.display ? "main-menu active" : "main-menu"}
+      >
         <nav className="g-nav">
           <ul>
             <li>
-              <Link href="#">商品一覧</Link>
+              <Link href="./">トップページ</Link>
             </li>
             <li>
-              <Link href="#">アクセス</Link>
+              <Link href="./items">商品一覧</Link>
             </li>
             <li>
-              <Link href="#">ギャラリー</Link>
+              <Link href="./access">アクセス</Link>
+            </li>
+            <li>
+              <Link href="./gallery">ギャラリー</Link>
             </li>
           </ul>
         </nav>
